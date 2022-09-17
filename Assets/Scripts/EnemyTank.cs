3d35a1df3c6afb60efef5a7c1f240e8b9906d8fa
Xwilarg <xwilarg@protonmail.com>
2022-09-17 19:43:58 +0200

    Add enemy

--- Assets/Scripts/EnemyTank.cs
+++ Assets/Scripts/EnemyTank.cs
@@ -1,9 +1,35 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 namespace OneHourJam386
 {
     public class EnemyTank : MonoBehaviour
     {
+        private NavMeshAgent _agent;
+
+        private GameObject[] _flags;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            GameObject.FindGameObjectsWithTag("Flag");
+        }
+
+        private void SetTask()
+        {
+            var r = _flags[Random.Range(0, _flags.Length)];
+            _agent.SetDestination(r.transform.position);
+        }
+
+        private void Start()
+        {
+            SetTask();
+        }
+
+        private void Update()
+        {
+            
+        }
     }
 }
